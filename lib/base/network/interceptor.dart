c8f574df4257b41debf3c9c26bd216ddd857a4f1
Sujal Shakya <sujalshakya387@gmail.com>
2024-09-15 11:01:20 +0545

    Fix: Minor Changes

--- lib/base/network/interceptor.dart
+++ lib/base/network/interceptor.dart
@@ -2,13 +2,14 @@ import 'dart:convert';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:stacked_services/stacked_services.dart';
 import 'package:whatsapp_stacked/app/app.locator.dart';
 import 'package:whatsapp_stacked/services/token_storage_service.dart';
 
 class DioInterceptor extends Interceptor {
   Dio dio = Dio();
-
   final _tokenService = locator<TokenStorageService>();
+  final _snackbarService = locator<SnackbarService>();
 
   /// Add headers to requests, include token when token is not null in secure storage.
   @override
@@ -42,8 +43,35 @@ class DioInterceptor extends Interceptor {
 
   /// Give feedback of type of error to user using snackbar.
   @override
-  Future<String> onError(err, handler) async {
+  Future<void> onError(err, handler) async {
     debugPrint(err.toString());
-    return err.toString();
+    debugPrint(err.toString());
+    switch (err.response?.statusCode) {
+      case 400:
+        _snackbarService.showSnackbar(
+            message: 'Bad Request', duration: const Duration(seconds: 2));
+      case 401:
+        _snackbarService.showSnackbar(
+            message: 'UnAuthorized', duration: const Duration(seconds: 2));
+      case 403:
+        _snackbarService.showSnackbar(
+            message: 'Forbidden', duration: const Duration(seconds: 2));
+      case 404:
+        _snackbarService.showSnackbar(
+            message: 'Not Found', duration: const Duration(seconds: 2));
+      case 429:
+        _snackbarService.showSnackbar(
+            message: 'Too Many Requests', duration: const Duration(seconds: 2));
+      case 502:
+        _snackbarService.showSnackbar(
+            message: 'Bad Gateway', duration: const Duration(seconds: 2));
+      case 504:
+        _snackbarService.showSnackbar(
+            message: 'Gateway Timeout', duration: const Duration(seconds: 2));
+      case 500:
+        _snackbarService.showSnackbar(
+            message: 'Internal Server Error',
+            duration: const Duration(seconds: 2));
+    }
   }
 }
